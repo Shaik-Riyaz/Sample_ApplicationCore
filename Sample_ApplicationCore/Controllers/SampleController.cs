8e5822e56eef6039b1d5f17402a2a6a267209926
Shaik Riyaz <riyaz.sk8@outlook.com>
2025-07-09 15:44:59 +0530

    Userstory: or (763456 )updated routing information in sample controller.

---
+++ Sample_ApplicationCore/Controllers/SampleController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sample_ApplicationCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+                //Riyaz
+    }
+}
+ 
